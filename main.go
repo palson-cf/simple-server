934b6af780acafb6588f2c5ec3db4f8e341c69c6
Pavel Nosovets <pavel.nosovets@codefresh.io>
2019-12-31 16:27:08 +0300

    Minor fix

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "The test %s service is working!", r.URL.Path[1:])
+    fmt.Fprintf(w, "The green %s service is working!", r.URL.Path[1:])
 }
 
 func main() {
