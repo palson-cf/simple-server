4254b3f069546ccfdc6d37a2955d4776f68f96b4
Pavel Nosovets <56305887+palson-cf@users.noreply.github.com>
2021-02-01 10:21:01 +0300

    Created new branch (#2)
    * Created new branch
    
    * Trigger build
    
    * Fake commitj

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "The green %s service is working!", r.URL.Path[1:])
+    fmt.Fprintf(w, "The super %s service is working!", r.URL.Path[1:])
 }
 
 func main() {
     http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe(":8081", nil))
 }
+
